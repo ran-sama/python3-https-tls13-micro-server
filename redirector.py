30725297a0ae1bb2791e3068a4a81e44837d6bd0
Yakumo Ran <31311154+ran-sama@users.noreply.github.com>
2025-02-09 11:50:18 +0100

    pylint recommendations

--- redirector.py
+++ redirector.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""Upgrade unsafe HTTP to HTTPS with HTTP 301 reply"""
+
 import os
+import sys
 from socketserver import ThreadingMixIn
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-MYSERV_ACMEWEBDIR = "/home/ran/.acmeweb"#must exist to run the script
+MYSERV_ACMEWEBDIR = "/home/ran/.acmeweb"  # must exist to run the script
+
 
 class RedirectHandler(SimpleHTTPRequestHandler):
+    """Handler for request types"""
     def do_HEAD(self):
-        if self.path.startswith("/.well-known"):#only serve acme challenges
+        """Serve a HEAD request"""
+        if self.path.startswith("/.well-known"):  # only serve acme challenges
             SimpleHTTPRequestHandler.do_HEAD(self)
         else:
             my_host = "localhost"
@@ -16,13 +23,14 @@ class RedirectHandler(SimpleHTTPRequestHandler):
             if 'Host' in self.headers:
                 my_host = self.headers.get('Host').split(':')[0]
                 my_path = self.path
-            self.send_response(301)#redirect all other requests
+            self.send_response(301)  # redirect all other requests
             self.send_header("Location", "https://" + my_host + my_path)
             self.send_header("Content-Length", "0")
             SimpleHTTPRequestHandler.end_headers(self)
 
     def do_GET(self):
-        if self.path.startswith("/.well-known"):#only serve acme challenges
+        """Serve a GET request"""
+        if self.path.startswith("/.well-known"):  # only serve acme challenges
             SimpleHTTPRequestHandler.do_GET(self)
         else:
             my_host = "localhost"
@@ -30,27 +38,34 @@ class RedirectHandler(SimpleHTTPRequestHandler):
             if 'Host' in self.headers:
                 my_host = self.headers.get('Host').split(':')[0]
                 my_path = self.path
-            self.send_response(301)#redirect all other requests
+            self.send_response(301)  # redirect all other requests
             self.send_header("Location", "https://" + my_host + my_path)
             self.send_header("Content-Length", "0")
             SimpleHTTPRequestHandler.end_headers(self)
 
+
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Allow multi-threading"""
     daemon_threads = True
 
+
 def main():
+    """Start server"""
     try:
-        os.chdir(MYSERV_ACMEWEBDIR)#auto-change working directory
-        SimpleHTTPRequestHandler.server_version = "nginx"#pretend to be nginx
-        SimpleHTTPRequestHandler.sys_version = ""#empty version string
+        sys.tracebacklimit = 0  # 1-line errorlog in production
+        os.chdir(MYSERV_ACMEWEBDIR)  # auto-change working directory
+        SimpleHTTPRequestHandler.server_version = "nginx"  # pretend to be nginx
+        SimpleHTTPRequestHandler.sys_version = ""  # empty version string
         server = ThreadedHTTPServer(('0.0.0.0', 80), RedirectHandler)
         print("Starting server, use <Ctrl-C> to stop")
         server.serve_forever()
-    except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError, TimeoutError):
-        pass
+    except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError, TimeoutError) as e:
+        print("caught: ", e)
+        # pass
     except KeyboardInterrupt:
         print(" received, shutting down server")
         server.shutdown()
+
 
 if __name__ == '__main__':
     main()
